9fae5dd8118a70fb427694a1dd1a266f3d37ceca
Honchar Yaroslav <honchar.yara@gmail.com>
2022-05-16 20:59:54 +0300

    v1.00.0

---
+++ script.js
@@ -0,0 +1,36 @@
+const refs = {
+  questionWindow: document.querySelector(".js-window"),
+  input: document.querySelector(".js-input"),
+  btnYes: document.querySelector(".js-btn-yes"),
+  btnNo: document.querySelector(".js-btn-no"),
+};
+
+refs.btnYes.addEventListener("mouseover", changeBtnPosition);
+refs.btnYes.addEventListener("mouseover", changeBtnPosition);
+refs.btnNo.addEventListener("click", clickBtnNoHandler);
+
+function clickBtnNoHandler() {
+  const input = refs.input;
+  const currentValue = input.value;
+  input.value = "Russian warship, f*ck yourself";
+  setTimeout(() => {
+    input.value = currentValue;
+    refs.btnYes.removeAttribute("style");
+  }, 3000);
+}
+
+function changeBtnPosition() {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const { width, height } = refs.btnYes.getBoundingClientRect();
+  const x = getRandomNumber(1, windowWidth - width);
+  const y = getRandomNumber(1, windowHeight - height);
+
+  refs.btnYes.style.position = "absolute";
+  refs.btnYes.style.top = `${y}px`;
+  refs.btnYes.style.left = `${x}px`;
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
